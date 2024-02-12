78f53132a48e1f6a3ddef73135ccf0bd12cc1850
Aytuğ <umut-aytug@hotmail.com>
2024-02-12 18:20:16 +0300

    ToDo: Fix the map

--- src/scripts/vueApp.js
+++ src/scripts/vueApp.js
@@ -5,6 +5,16 @@ createApp({
         const ipAddress = ref('');
         const ipInfo = ref(null);
 
+        const initMap = (latitude, longitude) => {
+            const map = L.map('map').setView([latitude, longitude], 13);
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+                maxZoom: 18,
+            }).addTo(map);
+
+            map.invalidateSize();
+        };
+
         const fetchIPInfo = async () => {
             try {
                 const response = await fetch(`https://ipapi.co/json/`);
@@ -16,23 +26,12 @@ createApp({
                 console.error('Error fetching IP information:', error);
             }
         };
-
         onMounted(fetchIPInfo);
-
-        const initMap = (latitude, longitude) => {
-            const map = L.map('map').setView([latitude, longitude], 10);
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
-            L.marker([latitude, longitude]).addTo(map)
-                .bindPopup('Your IP Location')
-                .openPopup();
-        };
 
         return {
             ipAddress,
             ipInfo,
-            initMap
         };
     }
 }).mount('#app');
+
